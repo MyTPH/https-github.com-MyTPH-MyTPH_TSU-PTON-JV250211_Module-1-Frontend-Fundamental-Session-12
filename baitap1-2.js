b7a3cd4054071071b232db6dc767a73b672ece00
MyTPH <tan.phamhongmy@gmail.com>
2025-03-17 01:06:01 +0900

    Add files via upload

---
+++ baitap1-2.js
@@ -0,0 +1,6 @@
+let age = prompt("Nhập tuổi của bạn:");
+if (age >= 15) {
+    console.log("Bạn đủ điều kiện vào học lớp 10.");
+} else {
+    console.log("Bạn KHÔNG đủ điều kiện vào học lớp 10.");
+}
