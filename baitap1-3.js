d76efdbe1dbe8de8c1dc605e5a95c1b64e561f22
MyTPH <tan.phamhongmy@gmail.com>
2025-03-17 01:06:41 +0900

    Add files via upload

---
+++ baitap1-3.js
@@ -0,0 +1,8 @@
+let number = prompt("Nhập một số nguyên:");
+if (number > 0) {
+    alert(number + " là số lớn hơn 0.");
+} else if (number < 0) {
+    alert(number + " là số nhỏ hơn 0.");
+} else {
+    alert("Số bạn nhập là 0.");
+}
